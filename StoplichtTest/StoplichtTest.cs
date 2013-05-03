8e61581dc2ca313b6bdf813aec843759460d10e8
Edsen <eklerk@gmail.com>
2013-05-03 15:50:27 +0200

    Minor changes

--- StoplichtTest/StoplichtTest.cs
+++ StoplichtTest/StoplichtTest.cs
@@ -66,53 +66,58 @@ namespace StoplichtTest
 
         static void TestConnection()
         {
-            // Specify the provider name, server and database.
-            string providerName = "System.Data.SqlClient";
-            string serverName = @"EDWIN-PC\SQLEXPRESS";
-            string databaseName = "SLService";
-
-            // Initialize the connection string builder for the
-            // underlying provider.
-            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
-
-            // Set the properties for the data source.
-            sqlBuilder.DataSource = serverName;
-            sqlBuilder.InitialCatalog = databaseName;
-            sqlBuilder.IntegratedSecurity = true;
-
-            // Build the SqlConnection connection string.
-            string providerString = sqlBuilder.ToString();
-
-            // Initialize the EntityConnectionStringBuilder.
-            EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder();
-
-            //Set the provider name.
-            entityBuilder.Provider = providerName;
-
-            // Set the provider-specific connection string.
-            entityBuilder.ProviderConnectionString = providerString;
-
-            // Set the Metadata location.
-            entityBuilder.Metadata = "res://DataLayer/Model.csdl|res://DataLayer/Model.ssdl|res://DataLayer/Model.msl";
-
-            Console.WriteLine(entityBuilder.ToString());
-
-            using (EntityConnection conn =
-            new EntityConnection(entityBuilder.ToString()))
+            try
             {
-                conn.Open();
+                // Specify the provider name, server and database.
+                string providerName = "System.Data.SqlClient";
+                string serverName = @"EDWIN-PC\SQLEXPRESS";
+                string databaseName = "SLService";
 
-                DataLayer.EventLog log = new DataLayer.EventLog();
-                log.Date = DateTime.Now;
-                log.Message = "Testing connection";
-                log.UserName = string.Join(";", StoplichtService.SystemInfo.CurrentActiveUsers());
-                DataLayer.SLServiceEntities sl = new DataLayer.SLServiceEntities(conn);
-                sl.AddToEventLog(log);
-                sl.SaveChanges();
+                // Initialize the connection string builder for the
+                // underlying provider.
+                SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
 
-                Console.WriteLine("Added 'Testing connection' to EventLog. Press any key to close.");
-                Console.Read();
-                conn.Close();
+                // Set the properties for the data source.
+                sqlBuilder.DataSource = serverName;
+                sqlBuilder.InitialCatalog = databaseName;
+                sqlBuilder.IntegratedSecurity = true;
+
+                // Build the SqlConnection connection string.
+                string providerString = sqlBuilder.ToString();
+
+                // Initialize the EntityConnectionStringBuilder.
+                EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder();
+
+                //Set the provider name.
+                entityBuilder.Provider = providerName;
+
+                // Set the provider-specific connection string.
+                entityBuilder.ProviderConnectionString = providerString;
+
+                // Set the Metadata location.
+                entityBuilder.Metadata = "res://DataLayer/Model.csdl|res://DataLayer/Model.ssdl|res://DataLayer/Model.msl";
+
+                Console.WriteLine(entityBuilder.ToString());
+
+                using (EntityConnection conn =
+                new EntityConnection(entityBuilder.ToString()))
+                {
+                    conn.Open();
+                    DataLayer.EventLog log = new DataLayer.EventLog();
+                    log.Date = DateTime.Now;
+                    log.Message = "Testing connection";
+                    log.UserName = string.Join(";", StoplichtService.SystemInfo.CurrentActiveUsers());
+                    DataLayer.SLServiceEntities sl = new DataLayer.SLServiceEntities(conn);
+                    sl.AddToEventLog(log);
+                    sl.SaveChanges();
+                    conn.Close();
+
+                    Console.WriteLine("Added 'Testing connection' to EventLog table.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Test failed: " + ex.Message);
             }
         }
     }
